396ccf7177ac90cce615bb4269c3fe9c8d822e1e
jung0han <jung0han@hanmail.net>
2020-11-23 19:24:18 +0900

    Add PostViewer

--- frontend/src/components/write/Editor.js
+++ frontend/src/components/write/Editor.js
@@ -32,7 +32,7 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
+const Editor = ({ title, body, onChangeField }) => {
   const quillElement = useRef(null);
   const quillInstance = useRef(null);
 
@@ -41,6 +41,8 @@ const Editor = () => {
       theme: 'bubble',
       placeholder: '내용을 작성하세요...',
       modules: {
+        // 더 많은 옵션
+        // https://quilljs.com/docs/modules/toolbar/ 참고
         toolbar: [
           [{ header: '1' }, { header: '2' }],
           ['bold', 'italic', 'underline', 'strike'],
@@ -49,11 +51,28 @@ const Editor = () => {
         ],
       },
     });
-  }, []);
+
+    // quill에 text-change 이벤트 핸들러 등록
+    // 참고: https://quilljs.com/docs/api/#events
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onChangeTitle = (e) => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
 
   return (
     <EditorBlock>
-      <TitleInput placeholder="제목을 입력하세요" />
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onChangeTitle}
+        value={title}
+      />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -3,15 +3,19 @@ import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import user, { userSaga } from './user';
 import loading from './loading';
+import write, { writeSaga } from './write';
+import post, { postSaga } from './post';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  write,
+  post,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
 }
 
 export default rootReducer;
